d49f2ce6a21a4d56920ae7cdcf357ab30ab489e5
omnisat <omnisats@protonmail.com>
2021-12-30 19:57:01 +0100

    add comments

--- client.py
+++ client.py
@@ -1,7 +1,11 @@
 import tweepy
+from tweepy.models import ResultSet, User, Status
 import datetime
 
 
+# Here is the client class that should parse all relevant tweets within the last 30 days. Iteration on the response
+# should be done to create models objects that can be store into a sqlite database using an ORM.
+#
 class Tw:
     def __init__(self):
         self.file = open("logs.txt", 'r')
@@ -10,6 +14,7 @@ class Tw:
         self.bearer_token = self.file.readline().split('\n')[0].split('=')[1]
         self.access_token = self.file.readline().split('\n')[0].split('=')[1]
         self.access_token_secret = self.file.readline().split('\n')[0].split('=')[1]
+        self.environment_label = self.file.readline().split('\n')[0].split('=')[1]
         self.file.close()
         self.v2_client = tweepy.Client(bearer_token=self.bearer_token, consumer_key=self.public_key,
                                        consumer_secret=self.secret_key, access_token=self.access_token,
@@ -19,16 +24,16 @@ class Tw:
         self.api = tweepy.API(self.auth)
 
     def search_last_7_days(self, query: str):
+        # Issues with V2 API as of end of 2021 : no information about each tweet for non-academical accounts
+        response = self.v2_client.search_recent_tweets(query=query, user_auth=True)
+        # next_token = r.meta['next_token']
+        return response
 
-        r = self.v2_client.search_recent_tweets(query=query, user_auth=True)
-        next_token = r.meta['next_token']
-
-        return r
-
-    def search_last_30_days(self, query: str):
-        r = self.api.search_30_day(label='dev', query=query)
-        return r
+    def search_last_30_days(self, query: str, max_results=10) -> ResultSet:
+        response = self.api.search_30_day(label=self.environment_label, query=query, maxResults=max_results)
+        return response
 
 
 client = Tw()
-r = client.search_last_30_days(query='"@CurveFinance" -to:CurveFinance')
+
+r = client.search_last_30_days(query='"@Curvance" -to:Curvance')
--- main.py
+++ main.py
@@ -1,3 +1,11 @@
 from client import Tw
 
-print('Hello Curve')
+# Main script to load / update tweets from DB/Client, create CurvanceUsers objects, and compute score using relevant
+# methods
+# Weight the sum of tweets scores by a User score that should be normalized among all users that participated.
+
+MAX_CVE_TO_DISTRIBUTE = 10000
+
+# ...
+
+print('Hello Curvance')
