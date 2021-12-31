fcd6fa5498f9528bdd64a83078a3cbffaed33333
omnisat <omnisats@protonmail.com>
2021-12-31 18:25:48 +0100

    Use peewee Models as base class for models

--- models.py
+++ models.py
@@ -1,25 +1,26 @@
-from tweepy.models import ResultSet, User, Status
+from tweepy.models import User, Status
+import peewee
 import datetime
-
 
 # Models for Users and Tweets
 # Theses classes should be derived of Base class of an ORM afterwards
 # They also need more methods to compute relevant metrics
 
-class CurvanceUser:
-    def __init__(self, tweepy_user_object: User):
-        self.user_id: int = tweepy_user_object.id
-        self.user_tag: str = tweepy_user_object.screen_name
-        self.account_creation_date: datetime.datetime = tweepy_user_object.created_at
-        self.followers_count: int = tweepy_user_object.followers_count
-        self.friends_count: int = tweepy_user_object.friends_count
-        self.statuses_count: int = tweepy_user_object.statuses_count
-        self.verified: bool = tweepy_user_object.verified
-        self.curvance_tweets: dict = {}
 
-    def add_tweet(self, tweepy_tweet_object: Status):
-        curvance_tweet = CurvanceTweet(tweepy_tweet_object)
-        self.curvance_tweets[curvance_tweet.tweet_id] = curvance_tweet
+db = peewee.SqliteDatabase('curvance_twitter.sqlite', pragmas={'foreign_keys': 1})
+
+
+class CurvanceUser(peewee.Model):
+    user_id: int = peewee.IntegerField(primary_key=True, unique=True)
+    user_tag: str = peewee.CharField()
+    account_creation_date: datetime.datetime = peewee.DateField()
+    followers_count: int = peewee.IntegerField()
+    friends_count: int = peewee.IntegerField()
+    statuses_count: int = peewee.IntegerField()
+    verified: bool = peewee.BooleanField()
+
+    class Meta:
+        database = db
 
     def compute_score(self):
         # Loop over all tweets and call CurvanceTweet.individual_score()
@@ -28,18 +29,21 @@ class CurvanceUser:
         pass
 
 
-class CurvanceTweet:
-    def __init__(self, tweepy_tweet_object: Status):
-        self.id: int = tweepy_tweet_object.id
-        self.date: datetime.datetime = tweepy_tweet_object.created_at
-        self.text: str = tweepy_tweet_object.text
-        self.source: str = tweepy_tweet_object.source
-        self.is_quote_status: bool = tweepy_tweet_object.is_quote_status
-        self.in_reply_to_screen_name: str = tweepy_tweet_object.in_reply_to_screen_name
-        self.quote_count: int = tweepy_tweet_object.quote_count
-        self.reply_count: int = tweepy_tweet_object.reply_count
-        self.retweet_count: int = tweepy_tweet_object.retweet_count
-        self.favorite_count: int = tweepy_tweet_object.favorite_count
+class CurvanceTweet(peewee.Model):
+    id: int = peewee.IntegerField(primary_key=True, unique=True)
+    date: datetime.datetime = peewee.DateField()
+    text: str = peewee.CharField()
+    source: str = peewee.CharField()
+    is_quote_status: bool = peewee.BooleanField()
+    in_reply_to_screen_name: str = peewee.CharField(null=True)
+    quote_count: int = peewee.IntegerField()
+    reply_count: int = peewee.IntegerField()
+    retweet_count: int = peewee.IntegerField()
+    favorite_count: int = peewee.IntegerField()
+    author = peewee.ForeignKeyField(CurvanceUser, backref='all_tweets')
+
+    class Meta:
+        database = db
 
     def individual_tweet_score(self) -> float:
         pass
